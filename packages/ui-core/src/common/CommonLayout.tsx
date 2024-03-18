ceb2ece34b6230474bdb2d0f8d9bfd2d37eb2cb4
Peter Koller <peter.koller@molitio.com>
2024-03-18 21:26:50 +0100

    chore(MWE-181): update to rollup and typescript configuration

--- packages/ui-core/src/common/CommonLayout.tsx
+++ packages/ui-core/src/common/CommonLayout.tsx
@@ -4,12 +4,9 @@ import React from 'react';
 const CommonLayout: React.FC<React.PropsWithChildren> = (props) => {
     const { children } = props;
     return (
-        <html lang="en">
+        <html>
             <head>
                 <title>MWE - John Glenn</title>
-                <meta charSet="UTF-8" />
-                <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
             </head>
             <body>
                 <header></header>
