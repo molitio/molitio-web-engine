7941babb66b0f808f5a9498859992cc907b9fe15
Peter Koller <peter.koller@molitio.com>
2024-03-24 17:47:52 +0100

    chore(MWE-181): updated schema structure, began basic implementation for one entity to test code gen and tooling

--- packages/system-schema/codegen.ts
+++ packages/system-schema/codegen.ts
@@ -2,8 +2,8 @@ import { CodegenConfig } from '@graphql-codegen/cli';
 
 const config: CodegenConfig = {
     generates: {
-        '../../.artifacts/ui-core/types.ts': { schema: '**/types/**/*.graphql', plugins: ['typescript'] },
-        '../../.artifacts/gql_client/': {
+        '../../.artifacts/gql/types/index.ts': { documents: '**/*.type.graphql', plugins: ['typescript'] },
+        '../../.artifacts/gql/gql_client/': {
             schema: './gql/root.graphql',
             preset: 'client',
         },
