8fdcf2a2aec9a17846b2dea9e08132b7ad5485e4
Peter Koller <peter.koller@molitio.com>
2024-04-07 17:33:11 +0200

    chore(MWE-181): updated build error

--- packages/resource-hub/src/user/user.resolver.ts
+++ packages/resource-hub/src/user/user.resolver.ts
@@ -1,6 +1,3 @@
-import { Resolver } from '@nestjs/graphql';
-import { UserPublic, UserPublicResolver } from '@molitio/system-schema';
+import { UserPublicResolver } from '@molitio/system-schema';
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-@Resolver((of) => UserPublic)
 export class UserResolver extends UserPublicResolver {}
--- packages/system-schema/src/ResourceHub/UserPublic/UserPublic.resolver.ts
+++ packages/system-schema/src/ResourceHub/UserPublic/UserPublic.resolver.ts
@@ -1,3 +1,8 @@
+import { Resolver } from '@nestjs/graphql';
+import { UserPublic } from '.';
+
+// TODO inject service
+@Resolver((of) => UserPublic)
 export default class UserPublicResolver {
     constructor() {}
 }
