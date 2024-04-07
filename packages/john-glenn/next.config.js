999d085c8a953b39483938c68c97c0c8b48c9292
Peter Koller <peter.koller@molitio.com>
2024-04-07 12:35:16 +0200

    chore(MWE-181): updated config

--- packages/john-glenn/next.config.js
+++ packages/john-glenn/next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-    output: 'standalone',
-    distDir: '../../.artifacts/john-glenn',
     //distDir: '.next',
     swcMinify: true,
     reactStrictMode: true,
