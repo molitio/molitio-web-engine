284526c72f8ea3ab0f6b27da4363206a06617fa9
Peter Koller <peter.koller@molitio.com>
2024-05-17 16:32:24 +0200

    chore(DB-12): navbar has now fixed position

--- packages-ui/ui-core/src/ui-nav-menu/NavBar.tsx
+++ packages-ui/ui-core/src/ui-nav-menu/NavBar.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const NavBar: React.FC = () => {
     return (
-        <div className="navbar bg-base-100">
+        <div className="navbar bg-base-100 fixed top-0 left-0">
             <div className="flex-1">
                 <a className="btn btn-ghost text-xl">daisyUI</a>
             </div>
