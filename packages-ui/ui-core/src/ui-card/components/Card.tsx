f802670b4cb46abe328bd0a270d5cd97c7a203f4
4E1ek <randm1212@gmail.com>
2024-08-06 16:55:38 +0200

    chore(MWE-185): Dashing Bumblebee Home page Card fix

--- packages-ui/ui-core/src/ui-card/components/Card.tsx
+++ packages-ui/ui-core/src/ui-card/components/Card.tsx
@@ -3,13 +3,13 @@ import { CardData } from '../types';
 import '../style/style.css';
 
 const Card: React.FC<CardData> = (props) => {
-    const { title, imageUrl, description } = props;
+    const { title, imageUrl, imageAlt, description } = props;
     return (
         <div className="flex flex-col items-center sm:gap-6 rounded-box m-4 p-4 text-white bg-gray-800">
             <div className="text-center max-w-xs ">
                 <div className="relative w-28 h-20 sm:w-28 sm:h-28 lg:w-32 lg:h-32 mx-auto">
                     <div className="z-10 relative w-full h-full rounded-full border border-gray-300 shadow flex items-center justify-center">
-                        {imageUrl}
+                        <img src={imageUrl} alt={imageAlt} className="w-full rounded-xl" />
                     </div>
                     <div className="absolute inset-0 -translate-x-2 -translate-y-2 rounded-full"></div>
                 </div>
