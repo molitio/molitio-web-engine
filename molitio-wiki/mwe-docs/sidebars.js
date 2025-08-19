a02785620a38fed712e57a5c3be1b51a46f57cd0
Peter Koller <peter.koller@molitio.com>
2025-08-19 14:31:38 +0200

    chore(MWE-205): added docusaurus for documentation automation

---
+++ molitio-wiki/mwe-docs/sidebars.js
@@ -0,0 +1,21 @@
+module.exports = {
+    docs: [
+        {
+            type: 'category',
+            label: 'Design System',
+            items: [
+                '../design-system/ui-card/plan',
+                '../design-system/ui-contact-form/plan',
+                '../design-system/ui-footer/plan',
+                '../design-system/ui-gallery-tool/plan',
+                '../design-system/ui-image-section/plan',
+                '../design-system/ui-interactive/plan',
+                '../design-system/ui-layout/plan',
+                '../design-system/ui-loading/plan',
+                '../design-system/ui-nav-bar/plan',
+                '../design-system/ui-page/plan',
+            ],
+        },
+        // Add more categories for other sections as needed
+    ],
+};
