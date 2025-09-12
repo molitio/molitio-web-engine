ccc3a53aaf337d1edcdd659a4b225a744beb56a5
mpatrikik <m.patrik01@gmail.com>
2025-09-12 15:40:20 +0200

    TaskContainer

--- apps-ui/john-glenn-ui/app/task/page.tsx
+++ apps-ui/john-glenn-ui/app/task/page.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import TaskContainer from '@/components/TaskContainer';
 
 const Task = () => {
     return (
         <div>
             Task
             <br />
+            <TaskContainer />
             <span className="text-purple-600">Practice Task App</span>
         </div>
     );
