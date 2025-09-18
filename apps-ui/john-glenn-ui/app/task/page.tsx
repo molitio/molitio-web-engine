6ae51fba575434925ace9bb284b2247a82eeaa0d
mpatrikik <m.patrik01@gmail.com>
2025-09-18 16:55:03 +0200

    running, lot of changes needed

--- apps-ui/john-glenn-ui/app/task/page.tsx
+++ apps-ui/john-glenn-ui/app/task/page.tsx
@@ -3,10 +3,12 @@ import './components/page.css';
 import TaskContainer from './components/TaskContainer';
 
 const Task = () => {
-    <div>
-        <h1>Simple Task Management Web App</h1>
-        <TaskContainer />
-    </div>;
+    return (
+        <div>
+            <h1>Simple Task Management Web App</h1>
+            <TaskContainer />
+        </div>
+    );
 };
 
 export default Task;
