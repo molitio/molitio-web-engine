e58d833c65bc7d651cdc240559b759c2b1e99ba5
Yogyerek1 <mszebi2008@gmail.com>
2025-10-04 11:10:22 +0200

    chore(MWE-183): updated packages

--- packages-ui/ui-core/src/core/components/CookieConsent.tsx
+++ packages-ui/ui-core/src/core/components/CookieConsent.tsx
@@ -1,30 +1,67 @@
-"use client"
+'use client';
 import React from 'react';
 import { useState } from 'react';
 
 const CookieConsent: React.FC = () => {
-    const [cookieAcceptAllButtonState, setCookieAcceptAllButtonState] = useState<"yes" | "no">("no");
-    const [cookieAcceptAdvertisementButtonState, setCookieAcceptAdvertisementButtonState] = useState<"yes" | "no">("no");
+    const [cookieAcceptAllButtonState, setCookieAcceptAllButtonState] = useState<'yes' | 'no'>('no');
+    const [cookieAcceptAdvertisementButtonState, setCookieAcceptAdvertisementButtonState] = useState<'yes' | 'no'>(
+        'no',
+    );
     return (
         <>
             <div className="fixed right-0 bottom-0 left-0 flex h-auto w-full max-w-2xl flex-col space-y-5 rounded-lg bg-blue-950 p-5 md:mx-auto md:h-auto md:w-2/3 lg:mx-auto lg:w-1/2">
-                <div className="grid grid-cols-1"><h3 className="text-left text-white">Cookie Concent</h3></div>
-                <div className="flex flex-wrap md:flex-wrap lg:flex-wrap"><h3 className="text-left text-white">We use cookies to improve your browsing experience, to show personalized content, and to analyze our traffic. You can choose to accept all cookies or manage your preferences.</h3></div>
+                <div className="grid grid-cols-1">
+                    <h3 className="text-left text-white">Cookie Concent</h3>
+                </div>
+                <div className="flex flex-wrap md:flex-wrap lg:flex-wrap">
+                    <h3 className="text-left text-white">
+                        We use cookies to improve your browsing experience, to show personalized content, and to analyze
+                        our traffic. You can choose to accept all cookies or manage your preferences.
+                    </h3>
+                </div>
 
                 <div className="space-y-7">
                     <div className="grid grid-cols-[1fr_auto] justify-between text-white">
                         <span className="text-left">Accept All Cookies</span>
                         <div className="flex space-x-5">
-                            <button className={`rounded p-1 px-2 hover:bg-gray-500 ${cookieAcceptAllButtonState === "yes" ? "bg-gray-500" : ""}`} onClick={() => setCookieAcceptAllButtonState("yes")}>Yes</button>
-                            <button className={`rounded p-1 px-2 hover:bg-gray-500 ${cookieAcceptAllButtonState === "no" ? "bg-gray-500" : ""}`} onClick={() => setCookieAcceptAllButtonState("no")}>No</button>
+                            <button
+                                className={`rounded p-1 px-2 hover:bg-gray-500 ${
+                                    cookieAcceptAllButtonState === 'yes' ? 'bg-gray-500' : ''
+                                }`}
+                                onClick={() => setCookieAcceptAllButtonState('yes')}
+                            >
+                                Yes
+                            </button>
+                            <button
+                                className={`rounded p-1 px-2 hover:bg-gray-500 ${
+                                    cookieAcceptAllButtonState === 'no' ? 'bg-gray-500' : ''
+                                }`}
+                                onClick={() => setCookieAcceptAllButtonState('no')}
+                            >
+                                No
+                            </button>
                         </div>
                     </div>
 
                     <div className="grid grid-cols-[1fr_auto] justify-between text-white">
                         <span className="text-left">Accept Advertisement Cookies</span>
                         <div className="flex space-x-5">
-                            <button className={`rounded p-1 px-2 hover:bg-gray-500 ${cookieAcceptAdvertisementButtonState === "yes" ? "bg-gray-500" : ""}`} onClick={() => setCookieAcceptAdvertisementButtonState("yes")}>Yes</button>
-                            <button className={`rounded p-1 px-2 hover:bg-gray-500 ${cookieAcceptAdvertisementButtonState === "no" ? "bg-gray-500" : ""}`} onClick={() => setCookieAcceptAdvertisementButtonState("no")}>No</button>
+                            <button
+                                className={`rounded p-1 px-2 hover:bg-gray-500 ${
+                                    cookieAcceptAdvertisementButtonState === 'yes' ? 'bg-gray-500' : ''
+                                }`}
+                                onClick={() => setCookieAcceptAdvertisementButtonState('yes')}
+                            >
+                                Yes
+                            </button>
+                            <button
+                                className={`rounded p-1 px-2 hover:bg-gray-500 ${
+                                    cookieAcceptAdvertisementButtonState === 'no' ? 'bg-gray-500' : ''
+                                }`}
+                                onClick={() => setCookieAcceptAdvertisementButtonState('no')}
+                            >
+                                No
+                            </button>
                         </div>
                     </div>
 
