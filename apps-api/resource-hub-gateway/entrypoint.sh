ba9ae1ebd86e23f6602e1f3064d32e5bcf77c21e
Peter Koller <peter.koller@molitio.com>
2025-10-12 11:19:11 +0200

    feat(MWE-191): docker compose local http environment configured and runs properly

--- apps-api/resource-hub-gateway/entrypoint.sh
+++ apps-api/resource-hub-gateway/entrypoint.sh
@@ -4,4 +4,3 @@ set -e
 echo "NGINX_PORT: $NGINX_PORT"
 echo "MWE_HOST: $MWE_HOST"
 envsubst '${NGINX_PORT} ${MWE_HOST}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
-nginx -g 'daemon off;'
