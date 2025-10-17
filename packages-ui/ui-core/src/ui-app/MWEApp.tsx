20f9c047cda5b505f51d4ef2b9220b837116a17b
Peter Koller <peter.koller@molitio.com>
2025-10-18 00:23:10 +0200

    chore(MWE-184): refactor to components and fixed tailwind config loading

---
+++ packages-ui/ui-core/src/ui-app/MWEApp.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MWEApp: React.FC = () => {
+    return (
+        <div className="bg-primary">
+            <h1>FRIDAY client</h1>
+        </div>
+    );
+};
+
+export default MWEApp;
