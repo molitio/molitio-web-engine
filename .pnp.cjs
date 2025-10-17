11b552584dff68667401f2c930ad414127d3ddca
Peter Koller <peter.koller@molitio.com>
2025-10-17 21:42:23 +0200

    chore(MWE-183): fixed lock file

--- .pnp.cjs
+++ .pnp.cjs
@@ -92,7 +92,6 @@ const RAW_RUNTIME_STATE =
           ["@parcel/watcher", "npm:2.4.1"],\
           ["@types/jest", "npm:29.5.4"],\
           ["@types/node", "npm:20.12.7"],\
-          ["@types/react", "npm:18.3.0"],\
           ["@types/ws", "npm:8.5.10"],\
           ["@typescript-eslint/eslint-plugin", "virtual:29c6370ad245650c0f5fa87161a75d060e622448ad21858b6a408ab5227f40ff97554acf6ca00f838ee24523955c3700504fb1b17e414e7c5d86ebc6744c5912#npm:8.46.0"],\
           ["@typescript-eslint/parser", "virtual:29c6370ad245650c0f5fa87161a75d060e622448ad21858b6a408ab5227f40ff97554acf6ca00f838ee24523955c3700504fb1b17e414e7c5d86ebc6744c5912#npm:8.46.0"],\
@@ -13486,7 +13485,6 @@ const RAW_RUNTIME_STATE =
           ["@parcel/watcher", "npm:2.4.1"],\
           ["@types/jest", "npm:29.5.4"],\
           ["@types/node", "npm:20.12.7"],\
-          ["@types/react", "npm:18.3.0"],\
           ["@types/ws", "npm:8.5.10"],\
           ["@typescript-eslint/eslint-plugin", "virtual:29c6370ad245650c0f5fa87161a75d060e622448ad21858b6a408ab5227f40ff97554acf6ca00f838ee24523955c3700504fb1b17e414e7c5d86ebc6744c5912#npm:8.46.0"],\
           ["@typescript-eslint/parser", "virtual:29c6370ad245650c0f5fa87161a75d060e622448ad21858b6a408ab5227f40ff97554acf6ca00f838ee24523955c3700504fb1b17e414e7c5d86ebc6744c5912#npm:8.46.0"],\
