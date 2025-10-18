1072ef9c1c3acf9f76e42d769f6c834ec74a5fd3
Peter Koller <peter.koller@molitio.com>
2025-10-18 10:32:03 +0200

    chore(MWE-184): tailwind now works with repo theme and app scope

--- apps-ui/john-glenn-ui/app/layout.tsx
+++ apps-ui/john-glenn-ui/app/layout.tsx
@@ -10,7 +10,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="en">
-            <body>
+            <body className="bg-primary min-h-screen">
                 <h1 className="text-blue-600">John Glenn</h1>
                 {children}
                 <CookieConsent />
