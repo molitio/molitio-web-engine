01bbebeed5773a7c78beaf01724d14642430fe9d
Peter Koller <peter.koller@molitio.com>
2025-10-20 09:20:50 +0200

    chore(MWE-184): reworked the design system, and extended examples of the use cases in john glenn

--- apps-ui/john-glenn-ui/app/layout.tsx
+++ apps-ui/john-glenn-ui/app/layout.tsx
@@ -10,8 +10,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="en">
-            <body className="bg-primary min-h-screen">
-                <h1 className="text-blue-600">John Glenn</h1>
+            <body className="bg-secondary min-h-screen">
                 {children}
                 <CookieConsent />
             </body>
--- packages-ui/ui-core/src/ui-interactive/components/Button.tsx
+++ packages-ui/ui-core/src/ui-interactive/components/Button.tsx
@@ -10,7 +10,7 @@ export const Button: React.FC<ButtonProps> = (props) => {
     const { children } = props;
     return (
         <button
-            className="align-middle text-center transition-all text-s py-3 px-6 rounded-lg bg-primary text-primary drop-shadow-sm shadow-primary disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none  hover:shadow-lg hover:shadow-primary focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none"
+            className="align-middle text-center transition-all text-s py-3 px-6 rounded-md bg-primary text-primary drop-shadow-sm shadow-primary border border-transparent disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none hover:border-accent hover:shadow-lg hover:shadow-primary focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none"
             type="button"
         >
             {children ?? ''}
