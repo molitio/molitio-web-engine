c492db7fd728e4fbb9834206459f3ea53394478d
Yogyerek1 <mszebi2008@gmail.com>
2025-11-01 19:31:04 +0100

    chore(MWE-183): added ButtonVariant

--- packages-ui/mwe-ui-core/src/ui-interactive/components/Button.tsx
+++ packages-ui/mwe-ui-core/src/ui-interactive/components/Button.tsx
@@ -1,12 +1,12 @@
 import { PropsWithChildren } from 'react';
-import { ButtonType } from '../types';
+import { ButtonVariant } from '../types';
 
 export type ButtonProps = {
-    type: ButtonType;
+    variant: ButtonVariant;
     onClick?: () => void;
 } & PropsWithChildren;
 
-export function Button({type, onClick, children}: ButtonProps) {
+export function Button({ children}: ButtonProps) {
     return (
         <button
             className="align-middle text-center transition-all text-s py-3 px-6 rounded-md bg-primary text-primary drop-shadow-sm shadow-primary border border-transparent disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none hover:border-accent hover:shadow-lg hover:shadow-primary focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none"
--- packages-ui/mwe-ui-core/src/ui-interactive/types/Button.ts
+++ packages-ui/mwe-ui-core/src/ui-interactive/types/Button.ts
@@ -1,4 +1,4 @@
-export enum ButtonType {
+export enum ButtonVariant {
     Primary,
     Secondary,
 }
