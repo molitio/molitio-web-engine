04c9764de689cc70d927446934e41b05ac2d3fe1
Yogyerek1 <mszebi2008@gmail.com>
2025-12-07 17:20:17 +0100

    chore(MWE-184): ui-nav-menu cleared

--- packages-ui/mwe-ui-core/src/ui-nav-menu/components/NavBar.tsx
+++ packages-ui/mwe-ui-core/src/ui-nav-menu/components/NavBar.tsx
@@ -1,16 +1,7 @@
-export type NavBarProps = {
-    headerText?: string;
-    navMenu: React.ReactNode;
-};
 
-export default function NavBar({ headerText, navMenu }: NavBarProps) {
+
+export default function NavBar() {
     return (
-        <nav className="sticky top-0 w-full z-5 flex direction-row items-start gap-2border border-primary">
-            <section className="flex direction-row gap-1 px-2">
-                <div>LOGO</div>
-                <h4 className="text-primary">{headerText}</h4>
-            </section>
-            {navMenu}
-        </nav>
+        <nav></nav>
     );
 }
--- packages-ui/mwe-ui-core/src/ui-nav-menu/components/NavMenu.tsx
+++ packages-ui/mwe-ui-core/src/ui-nav-menu/components/NavMenu.tsx
@@ -1,31 +1,7 @@
-import { Link } from '@tanstack/react-router';
-import { SupportedLocale } from '../../ui-app/constants';
 
-export type NavMenuProps = {
-    locale: SupportedLocale;
-} & { children?: React.ReactNode };
 
-export default function NavMenu({ locale, children }: NavMenuProps) {
+export default function NavMenu() {
     return (
-        <section className="flex-1 flex items-center gap-3 justify-start">
-            <ul className="flex direction-row justify-start gap-2">
-                <li>
-                    <Link to="/$locale" params={{ locale }} activeProps={{ className: 'font-bold' }}>
-                        Home
-                    </Link>
-                </li>
-                <li>
-                    <Link to="/$locale/about" params={{ locale }} activeProps={{ className: 'font-bold' }}>
-                        About
-                    </Link>
-                </li>
-                <li>
-                    <Link to="/$locale/contact" params={{ locale }} activeProps={{ className: 'font-bold' }}>
-                        Contact
-                    </Link>
-                </li>
-            </ul>
-            <div className="justify-end text-end flex-1 border border-accent">{children}</div>
-        </section>
+        <nav></nav>
     );
 }
